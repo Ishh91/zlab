f643aa42e9caf0bfca71336ced69d9e8d0e85618
Ishh91 <ishantsharma9105@gmail.com>
2026-08-17 20:17:12 +0530

    tjhgjnhg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
-// import Navbar from '@/components/Navbar';
-// import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 import Home from '@/pages/Home';
 import About from '@/pages/About';
 import Services from '@/pages/Services';
